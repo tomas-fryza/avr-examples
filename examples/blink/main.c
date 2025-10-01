9ffa4673674ac7133edf8cc48d0572cf8d3b5c5b
tomas-fryza <tomas.fryza@gmail.com>
2025-10-01 19:52:30 +0200

    blink example simplified

--- examples/blink/main.c
+++ examples/blink/main.c
@@ -1,25 +1,21 @@
 /*
  * Blink a LED using delay.
- * (c) 2018-2024 Tomas Fryza, MIT license
+ * (c) 2018-2025 Tomas Fryza, MIT license
  *
- * Developed using PlatformIO and AVR 8-bit Toolchain 3.6.2.
+ * Developed using PlatformIO and Atmel AVR plafform.
  * Tested on Arduino Uno board and ATmega328P, 16 MHz.
  */
 
-// -- Defines --------------------------------------------------------
-#define LED_BUILTIN PB5  // On-board LED
-#define SHORT_DELAY 250  // Delay in milliseconds
-#ifndef F_CPU
-#define F_CPU 16000000   // For delay
-#endif
+// -- Defines ----------------------------------------------
+#define LED PB5  // On-board LED pin
 
 
-// -- Includes -------------------------------------------------------
+// -- Includes ---------------------------------------------
 #include <avr/io.h>      // AVR device-specific IO definitions
 #include <util/delay.h>  // Functions for busy-wait delay loops
 
 
-// -- Function definitions -------------------------------------------
+// -- Function definitions ---------------------------------
 /*
  * Function: Main function where the program execution begins
  * Purpose:  Toggle LED using delay.
@@ -28,17 +24,17 @@
 int main(void)
 {
     // Set output pin in Data Direction Register
-    DDRB = DDRB | (1 << LED_BUILTIN);
+    DDRB = DDRB | (1 << LED);
     // Set pin LOW in Data Register (LED off)
-    PORTB = PORTB & ~(1 << LED_BUILTIN);
+    PORTB = PORTB & ~(1 << LED);
 
     // Infinite loop
     while (1)
     {
         // Pause several milliseconds
-        _delay_ms(SHORT_DELAY);
+        _delay_ms(500);
         // Invert LED in Data Register
-        PORTB = PORTB ^ (1 << LED_BUILTIN);
+        PORTB = PORTB ^ (1 << LED);
     }
 
     // Will never reach this
